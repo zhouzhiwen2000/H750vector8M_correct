980681b78a2761b4154c10d4c542c772b2c4b6f0
zhouzhiwen2000 <zhouzhiwen2000@gmail.com>
2021-04-02 17:58:37 +0800

    shrink

--- MDK-ARM/h750/datascope_dp.d
+++
@@ -1,2 +0,0 @@
-h750/datascope_dp.o: ..\Tuning_Utils\DataScope_DP.C \
-  ..\Tuning_Utils\DataScope_DP.h
--- MDK-ARM/h750/scsprotocol.d
+++
@@ -1,4 +0,0 @@
-h750/scsprotocol.o: ..\Hardware\SCSProtocol.c \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
-  ..\Hardware\SCSProtocol.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
